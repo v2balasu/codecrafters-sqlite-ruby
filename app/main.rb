26b10121e529e5d958dd1f1bca9e534a3a16f88a
v2balasu <vithuranb@gmail.com>
2024-11-10 11:25:56 -0500

    Read num pages from schema

--- app/main.rb
+++ app/main.rb
@@ -8,6 +8,17 @@ if command == ".dbinfo"
 
     database_file.seek(16)  # Skip the first 16 bytes of the header
     page_size = database_file.read(2).unpack("n")[0]
+
+    # Skip rest of dabase file header
+    database_file.pos += 82
+
+    # Assumptions: 
+    # * no free pages
+    # * first b tree page is sqlite_schema
+    database_file.pos += 3
+    num_tables = database_file.read(2).unpack("n")[0]
+
     puts "database page size: #{page_size}"
+    puts "number of tables: #{num_tables}"
   end
 end
