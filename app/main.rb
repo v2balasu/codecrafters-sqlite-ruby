87924010362272f90cedc6d45166ed6d640b2e49
v2balasu <vithuranb@gmail.com>
2024-11-10 16:29:35 -0500

    refactor

--- app/main.rb
+++ app/main.rb
@@ -1,24 +1,32 @@
-database_file_path = ARGV[0]
-command = ARGV[1]
+def main
+  database_file_path = ARGV[0]
+  command = ARGV[1]
 
-if command == ".dbinfo"
-  File.open(database_file_path, "rb") do |database_file|
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    $stderr.puts "Logs from your program will appear here"
+  case command
+  when '.dbinfo'
+    dbinfo(database_file_path)
+  else
+    raise 'Invalid command'
+  end
+end
 
-    database_file.seek(16)  # Skip the first 16 bytes of the header
-    page_size = database_file.read(2).unpack("n")[0]
+def dbinfo(database_file_path)
+  File.open(database_file_path, 'rb') do |database_file|
+    database_file.seek(16) # Skip the first 16 bytes of the header
+    page_size = database_file.read(2).unpack1('n')
 
     # Skip rest of dabase file header
     database_file.pos += 82
 
-    # Assumptions: 
+    # Assumptions:
     # * no free pages
     # * first b tree page is sqlite_schema
     database_file.pos += 3
-    num_tables = database_file.read(2).unpack("n")[0]
+    num_tables = database_file.read(2).unpack1('n')
 
     puts "database page size: #{page_size}"
     puts "number of tables: #{num_tables}"
   end
 end
+
+main
